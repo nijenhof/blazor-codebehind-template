0f5f6eba843afa4cdffeb51d6b21eb9e36503dbf
Micha Nijenhof <killermi200@gmail.com>
2021-06-02 12:45:47 +0200

    Add files via upload

---
+++ Index.razor.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace $rootnamespace$
+{
+    public partial class $safeitemname$
+    {
+        protected override Task OnInitializedAsync()
+        {
+            return base.OnInitializedAsync();
+        }
+    }
+}
